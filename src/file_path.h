dbf3a38d6da5773384e29d5fd5581909a3fb1405
Soucelover <mr.ktotov@gmail.com>
2024-06-22 12:11:33 +0300

    Separate path parts implementation

--- src/file_path.h
+++ src/file_path.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <stdexcept>
 
 namespace virtfiles
@@ -7,254 +8,198 @@ namespace virtfiles
 	{
 	protected:
 		std::string path_string;
-		size_t parts_count;
-		const char* const* parts = nullptr;
 
 	public:
-		size_t get_parts_count()
+		class parts_t
 		{
-			return parts_count;
-		}
+		protected:
+			const char* const* items;
+			size_t count;
+			const char* items_str;
+			size_t str_size;
 
-		class iterator;
+		public:
+			class iterator;
 
-		path_t()
-			: path_string(""), parts_count(1),
-			parts(new char* [1]) {}
+			constexpr size_t get_count() const
+			{
+				return count;
+			}
 
-		path_t(const char* path)
-		{
-			init(static_cast<std::string>(path));
-		}
+			constexpr const char* str() const
+			{
+				return items_str;
+			}
+
+			parts_t()
+				: items(nullptr), count(0), items_str(nullptr), str_size(0)
+			{
+			}
+
+			parts_t(const parts_t& other)
+			{
+				init(other);
+			}
+
+			parts_t(parts_t&& other) noexcept
+				: items(other.items), count(other.count),
+				items_str(other.items_str), str_size(other.str_size)
+			{
+				other.items = nullptr;
+				other.items_str = nullptr;
+			}
+
+			parts_t& operator=(const parts_t& other)
+			{
+				init(other);
+
+				return *this;
+			}
+
+			parts_t& operator=(parts_t&& other) noexcept
+			{
+				items = other.items;
+				count = other.count;
+				items_str = other.items_str;
+				str_size = other.str_size;
+
+				other.items = nullptr;
+				other.items_str = nullptr;
+
+				return *this;
+			}
+
+			void init(const std::string& path)
+			{
+				// Copy path string
+				str_size = path.length();
+				char* str = new char[str_size + 1] {};
+				items_str = str;
+
+				memcpy(str, path.data(), str_size);
+
+				// First item starts at first string character
+				std::vector<const char*> out = { str };
+
+				// Iterate over path
+				for (char* end_str = str + str_size; str < end_str; ++str)
+				{
+					// If it's part separator
+					// then append new part
+					switch (*str)
+					{
+					case '/':
+					case '\\':
+						// Finish previous part by NULL character
+						*str = NULL;
+						out.push_back(str + 1);
+					}
+				}
+
+				// Copy vector to items
+				count = out.size();
+				const char** items;
+				this->items = items = new const char* [count];
+
+				memcpy(items, out.data(), count * sizeof(const char*));
+			}
+
+			void init(const parts_t& other)
+			{
+				if (items_str)
+				{
+					delete[] items_str;
+					delete[] items;
+				}
+
+				count = other.count;
+				str_size = other.str_size;
+
+				const char** items;
+				this->items = items = new const char* [count];
+
+				char* items_str;
+				this->items_str = items_str = new char[str_size];
+
+				memcpy(items_str, other.items_str, str_size);
+
+				for (size_t i = 0; i < count; ++i)
+				{
+					items[i] = items_str + (other.items_str - other.items[i]);
+				}
+			}
+
+			constexpr const char* const& operator[] (long long index) const
+			{
+				return items[index];
+			}
+
+			iterator begin() const;
+
+			iterator end() const;
+		};
+
+		parts_t parts;
+
+		path_t() = default;
 
 		path_t(const std::string& path)
 		{
-			init(path);
+			path_string = path;
+			parts.init(path);
 		}
 
-		path_t(const path_t& other)
-		{
-			init(other);
-		}
-
-		path_t(path_t&& other) noexcept
-		{
-			path_string = other.path_string;
-			parts_count = other.parts_count;
-			parts = other.parts;
-			other.parts = nullptr;
-			other.path_string = "";
-			other.parts_count = 0;
-		}
-
-		~path_t()
-		{
-			delete[] parts[0];
-			delete[] parts;
-		}
-
-		path_t& operator=(_In_z_ const char* path)
-		{
-			init(static_cast<std::string>(path));
-
-			return *this;
-		}
-
-		path_t& operator=(_In_z_ const std::string& path)
-		{
-			init(path);
-
-			return *this;
-		}
-
-		path_t& operator=(_In_ const path_t& other)
-		{
-			init(other);
-
-			return *this;
-		}
-
-		path_t& operator=(_In_ path_t&& other) noexcept
-		{
-			if (parts)
-			{
-				delete[] parts[0];
-				delete[] parts;
-			}
-
-			path_string = other.path_string;
-			parts_count = other.parts_count;
-			parts = other.parts;
-
-			other.parts = nullptr;
-			other.path_string = "";
-			other.parts_count = 0;
-
-			return *this;
-		}
-
-		void init(const std::string& path)
+		path_t& operator=(const std::string& path)
 		{
 			path_string = path;
+			parts.init(path);
 
-			if (parts)
-			{
-				delete[] parts[0];
-				delete[] parts;
-			}
-
-			parts_count = get_parts_count(path);
-			const char** parts = new const char* [parts_count];
-
-			parse_parts(parts, path, parts_count);
-			this->parts = parts;
+			return *this;
 		}
 
-		void init(const path_t& other)
-		{
-			if (parts)
-			{
-				delete[] parts[0];
-				delete[] parts;
-			}
-
-			path_string = other.path_string;
-			parts_count = other.parts_count;
-
-			char** parts = new char* [parts_count];
-			size_t size = other.parts[parts_count - 1] - other.parts[0] + strlen(other.parts[parts_count - 1]);
-
-			parts[0] = new char[size];
-			memcpy(parts[0], other.parts[0], size);
-
-			for (size_t i = 1; i < parts_count; ++i)
-			{
-				parts[i] = parts[i - 1] + (other.parts[i] - other.parts[i - 1]);
-			}
-
-			this->parts = parts;
-		}
-
-		static size_t get_parts_count(const std::string& path)
-		{
-			size_t out = 1;
-
-			for (char c : path)
-			{
-				if (c == '\\' || c == '/')
-				{
-					out++;
-				}
-			}
-
-			return out;
-		}
-
-		static void parse_parts(_Out_writes_(count) const char** out,
-			_In_ const std::string& path, _In_ size_t count)
-		{
-			// copy path string
-			char* out_str = new char[path.length() + 1];
-			memcpy(out_str, path.data(), path.length());
-			out_str[path.length()] = '\0';
-
-			// initialize pointers
-			char** ptr = const_cast<char**>(out);
-			char** last = ptr + count - 1;
-
-			// first element point to start of out_str
-			*ptr = out_str;
-
-			if (count <= 1)
-			{
-				return;
-			}
-
-			*(ptr + 1) = out_str; // second element point to it too
-
-			while (true)
-			{
-				// handle current char
-				switch (**(ptr + 1))
-				{
-				case '/':
-				case '\\':
-					**(ptr + 1) = '\0'; // replace separator by null char
-					++ptr;				// next ptr
-					++*ptr;				// set ptr to next past null char
-
-					if (ptr < last)
-					{
-						*(ptr + 1) = *ptr; // set next pointer
-					}
-					else
-					{
-						return;
-					}
-				}
-				++ * (ptr + 1); // next symbol
-			}
-		}
-
-		iterator begin() const;
-
-		iterator end() const;
-
-		const char* operator[](long long index)
-		{
-			if (0 > index || index >= parts_count)
-			{
-				throw std::out_of_range("fs_path::operator[] index arg is out of range.");
-			}
-
-			return parts[index];
-		}
-
-		const std::string& str() const
+		constexpr const std::string& str() const
 		{
 			return path_string;
 		}
-
-		size_t get_parts_count() const
-		{
-			return parts_count;
-		}
 	};
 
-	class path_t::iterator : public std::iterator<
+	class path_t::parts_t::iterator : public std::iterator<
 		std::random_access_iterator_tag,
 		const char*,
 		long long,
 		const char* const*,
 		const char* const&>
 	{
-		friend path_t;
 		friend iterator operator+(long long, const iterator&);
+	protected:
+		const path_t::parts_t* parts;
+
+		long long index;
 
 	public:
-		explicit iterator(const path_t& path)
-			: path(std::addressof(path)), index(0)
+		explicit iterator(const path_t::parts_t& parts)
+			: parts(std::addressof(parts)), index(0)
 		{
 		}
 
-		iterator(const path_t* path, size_t index)
-			: path(path), index(index)
+		iterator(const path_t::parts_t* parts, size_t index)
+			: parts(parts), index(index)
 		{
 		}
 
 		iterator(const iterator& other)
-			: path(other.path), index(other.index)
+			: parts(other.parts), index(other.index)
 		{
 		}
 
 		iterator(iterator&& other) noexcept
-			: path(other.path), index(other.index)
+			: parts(other.parts), index(other.index)
 		{
 		}
 
 		iterator& operator=(const iterator& other)
 		{
-			path = other.path;
+			parts = other.parts;
 			index = other.index;
 
 			return *this;
@@ -262,7 +207,7 @@ namespace virtfiles
 
 		iterator& operator=(iterator&& other) noexcept
 		{
-			path = other.path;
+			parts = other.parts;
 			index = other.index;
 
 			return *this;
@@ -274,16 +219,16 @@ namespace virtfiles
 
 		void swap(const iterator& other)
 		{
-			const path_t* parent_sav = path;
+			const parts_t* parent_sav = parts;
 			size_t index_sav = index;
 
 			*this = other;
 
-			const_cast<const path_t*&>(other.path) = parent_sav;
+			const_cast<const parts_t*&>(other.parts) = parent_sav;
 			const_cast<long long&>(other.index) = index_sav;
 		}
 
-		iterator& operator++()
+		constexpr iterator& operator++()
 		{
 			++index;
 			return *this;
@@ -296,7 +241,7 @@ namespace virtfiles
 			return sav;
 		}
 
-		iterator& operator--()
+		constexpr iterator& operator--()
 		{
 			--index;
 			return *this;
@@ -309,22 +254,22 @@ namespace virtfiles
 			return sav;
 		}
 
-		reference operator*() const
+		constexpr reference operator*() const
 		{
-			return path->parts[index];
+			return (*parts)[index];
 		}
 
-		bool operator==(const iterator& other) const
+		constexpr bool operator==(const iterator& other) const
 		{
-			return path == other.path && index == other.index;
+			return parts == other.parts && index == other.index;
 		}
 
-		bool operator!=(const iterator& other) const
+		constexpr bool operator!=(const iterator& other) const
 		{
 			return !(*this == other);
 		}
 
-		iterator& operator+=(long long diff)
+		constexpr iterator& operator+=(long long diff)
 		{
 			index += diff;
 			return *this;
@@ -332,10 +277,10 @@ namespace virtfiles
 
 		iterator operator+(long long diff)
 		{
-			return iterator(path, index + diff);
+			return iterator(parts, index + diff);
 		}
 
-		iterator& operator-=(long long diff)
+		constexpr iterator& operator-=(long long diff)
 		{
 			index -= diff;
 			return *this;
@@ -343,7 +288,7 @@ namespace virtfiles
 
 		iterator operator-(long long diff)
 		{
-			return iterator(path, index - diff);
+			return iterator(parts, index - diff);
 		}
 
 		long long operator-(const iterator& other)
@@ -351,66 +296,61 @@ namespace virtfiles
 			return index - other.index;
 		}
 
-		reference operator[](long long idx)
+		constexpr reference operator[](long long idx)
 		{
-			return path->parts[index + idx];
+			return (*parts)[index + idx];
 		}
 
-		bool operator<(const iterator& other)
+		constexpr bool operator<(const iterator& other)
 		{
 			return index < other.index;
 		}
 
-		bool operator>(const iterator& other)
+		constexpr bool operator>(const iterator& other)
 		{
 			return index > other.index;
 		}
 
-		bool operator<=(const iterator& other)
+		constexpr bool operator<=(const iterator& other)
 		{
 			return index <= other.index;
 		}
 
-		bool operator>=(const iterator& other)
+		constexpr bool operator>=(const iterator& other)
 		{
 			return index >= other.index;
 		}
 
-		const path_t& get_path() const
+		constexpr const path_t::parts_t& get_path() const
 		{
-			return *path;
+			return *parts;
 		}
 
-		long long get_index() const
+		constexpr long long get_index() const
 		{
 			return index;
 		}
-
-	protected:
-		const path_t* path;
-
-		long long index;
 	};
 
-	path_t::iterator path_t::begin() const
+	path_t::parts_t::iterator path_t::parts_t::begin() const
 	{
-		return path_t::iterator(*this);
+		return iterator(*this);
 	}
 
-	path_t::iterator path_t::end() const
+	path_t::parts_t::iterator path_t::parts_t::end() const
 	{
-		return path_t::iterator(this, parts_count);
+		return iterator(this, get_count());
 	}
 
-	extern path_t::iterator operator+(long long diff, const path_t::iterator& iter)
+	extern path_t::parts_t::iterator operator+(long long diff, const path_t::parts_t::iterator& iter)
 	{
-		return virtfiles::path_t::iterator(iter.path, iter.index + diff);
+		return virtfiles::path_t::parts_t::iterator(iter.parts, iter.index + diff);
 	}
 };
 
 namespace std
 {
-	void swap(virtfiles::path_t::iterator left, virtfiles::path_t::iterator right)
+	void swap(virtfiles::path_t::parts_t::iterator left, virtfiles::path_t::parts_t::iterator right)
 	{
 		left.swap(right);
 	}
--- src/main.h
+++ src/main.h
@@ -1,1 +1,1 @@
-#include <file_path.h>
+#include "file_path.h"
